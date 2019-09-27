4a0f8a7b6e88e01b6a450ec79ea54474c47a0846
Matthew Nespor <MNespor@gannett.com>
2019-09-27 15:45:36 -0500

    initial commit

---
+++ .github/actions/validate-json/index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const core = require('@actions/core')
+const { promisify } = require('util')
+const fs = require('fs')
+
+(async () => {
+  try {
+    core.setOutput('message', 'Success')
+  } catch(error) {
+    core.setFailed(error.message)
+  }
+})()

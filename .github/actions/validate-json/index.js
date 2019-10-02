f558ae816eda3b7ea33326068f09484903a90606
Matthew Nespor <MNespor@gannett.com>
2019-10-02 17:04:07 -0500

    do stuff

--- .github/actions/validate-json/index.js
+++ .github/actions/validate-json/index.js
@@ -1,18 +1,48 @@
 'use strict'
 
 const core = require('@actions/core')
-const { promisify } = require('util')
-const fs = require('fs')
+const fs = require('fs').promises
+const { join } = require('path')
+
+async function find(path, predicate) {
+  const paths = []
+  await walk(path, filePath => {
+    if (predicate(filePath)) {
+      paths.push(filePath)
+    }
+  })
+
+  return paths
+}
+
+async function walk(path, callback) {
+  for (let dirent of await fs.readdir(path, { withFileTypes: true })) {
+    if (dirent.name === '.git' || dirent.name == '.github') {
+      continue
+    }
+
+    if (dirent.isDirectory()) {
+      await walk(join(path, dirent.name), callback)
+    } else if (dirent.isFile()) {
+      callback(join(path, dirent.name))
+    }
+  }
+}
 
 ;(async () => {
-  try {
-    core.setOutput('message', `Working directory: ${process.cwd()}; File location: ${__filename}`)
-    
-  } catch(error) {
-    core.setFailed(error.message)
+  const paths = await find('.', path => /\.json$/.test(path))
+  const errors = []
+  for (let path of paths) {
+    try {
+      JSON.parse(await fs.readFile(path, 'utf8'))
+    } catch (error) {
+      errors.push(`${path}: ${error.message}`)
+    }
+  }
+
+  if (errors.length) {
+    core.setFailed(errors.join('\n'))
+  } else {
+    core.setOutput('message', 'Success')
   }
 })()
-
-async function walk() {
-  
-}
